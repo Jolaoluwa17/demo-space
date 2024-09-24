2be75c1b79b9f4c3b241e5d757d72caea90152f3
Olusanya <olusanyajolaoluwa@gmail.com>
2024-09-24 12:05:42 +0100

    updates

---
+++ src/components/home/theFuture/TheFuture.tsx
@@ -0,0 +1,34 @@
+import './theFuture.css';
+
+const TheFuture = () => {
+  return (
+    <div className="the_future_root">
+      <div className="the_future_container">
+        <div className="left">
+          <div className="the_future_title">
+            The Future of <span>Skill Evaluation and Career</span> Growth
+          </div>
+          <div className="the_future_subTitle">
+            Go borderless on mobile! Download the app and sign up to assess your
+            skills, bridge gaps, and unlock new career opportunities from
+            anywhere in the world.
+          </div>
+          <div className="the_future_download_our">Download our app</div>
+          <div className="the_future_download_btn_conatiner">
+            <div className="the_future_download_btn">
+              <img src="/images/AppStore.svg" alt="" className='btn_img' />
+            </div>
+            <div className="the_future_download_btn">
+              <img src="/images/PlayStore.svg" alt="" className='btn_img' />
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <img src="/images/TheFuture.svg" alt="" className='the_future_phone_img' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TheFuture;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,8 @@ import AppFeatures from '../../components/home/appFeatures/AppFeatures';
 import ExpandPotential from '../../components/home/expandPotential/ExpandPotential';
 import HeroSection from '../../components/home/heroSection/HeroSection';
 import AskQuestions from '../../components/home/questions/Questions';
+import TheFuture from '../../components/home/theFuture/TheFuture';
+import { LuCopyright } from 'react-icons/lu';
 import './home.css';
 
 const Home = () => {
@@ -13,6 +15,10 @@ const Home = () => {
       <AppFeatures />
       <ExpandPotential />
       <AskQuestions />
+      <TheFuture />
+      <div className="home_footer">
+        <LuCopyright /> Copyright 2024. All Right Reserved.
+      </div>
     </div>
   );
 };
