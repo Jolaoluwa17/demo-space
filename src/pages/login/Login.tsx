feb3b92b7caa651c9f9df1efed61b8605b4f7125
Olusanya <olusanyajolaoluwa@gmail.com>
2024-11-18 12:28:32 +0100

    updates

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -91,107 +91,16 @@ const Login = () => {
 
   return (
     <div className="login_root">
+      <div className="techwings_logo_login">
+        <img
+          src="/assets/images/TechWingLogo.svg"
+          alt="login_image"
+          onClick={() => navigator('/')}
+        />
+      </div>
       <div className="login_container">
-        <div className="left_section">
-          <div className="techwings_logo_login">
-            <img
-              src="/assets/images/TechWingLogo.svg"
-              alt="login_image"
-              onClick={() => navigator('/')}
-            />
-          </div>
-          <div className="login_form">
-            <div className="login_title">Login</div>
-            <p>
-              Please provide the information below to login and access your
-              account
-            </p>
-            <div className="form_item">
-              <label htmlFor="email">Enter Email</label>
-              <input
-                type="email"
-                name="email"
-                placeholder="ysemiraefe@gmail.com"
-                className="input"
-                value={email}
-                onChange={(e) => handleEmailChange(e.target.value)}
-              />
-            </div>
-            <div className="form_item">
-              <label htmlFor="password">Enter Password</label>
-              <div className="password_input">
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  name="password"
-                  placeholder="************"
-                  className="input"
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                <div onClick={handleHiddenTrigger} className="see_password">
-                  {showPassword ? <EyeOpen /> : <EyeClosed />}
-                </div>
-              </div>
-            </div>
-            <div className="others">
-              <div className="rememberMe">
-                <input type="checkbox" style={{ cursor: 'pointer' }} />
-                <div style={{ marginLeft: '8px' }}>Remember me</div>
-              </div>
-              <div
-                style={{ color: '#FF8682', cursor: 'pointer' }}
-                onClick={() => navigator('/auth/forgotpassword')}
-                className="forgot_password_text"
-              >
-                Forgot Password
-              </div>
-            </div>
-            <button
-              className="login_btn"
-              style={{
-                backgroundColor: isFormValid && !isLoading ? '#4274BA' : 'grey',
-                cursor: isFormValid && !isLoading ? 'pointer' : 'not-allowed',
-              }}
-              onClick={handleLogin}
-              disabled={!isFormValid || isLoading}
-            >
-              {isLoading ? <div className="spinner"></div> : 'Login'}
-            </button>
-            <div className="doAccount">
-              Don’t have an account?{' '}
-              <span
-                style={{
-                  color: '#FF8682',
-                  fontWeight: '600',
-                  cursor: 'pointer',
-                }}
-                onClick={() => navigator('/auth/signup')}
-              >
-                Sign up
-              </span>
-            </div>
-            <div className="or_login">
-              <hr />
-              <div>Or login with</div>
-              <hr />
-            </div>
-            <div className="other_login_options">
-              <div className="login_options">
-                <img src="/assets/images/Google.svg" alt="login_image" />
-              </div>
-              <div className="login_options">
-                <img src="/assets/images/Apple.svg" alt="login_image" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="right_section">
-          <img
-            src="/assets/images/Login.svg"
-            alt="login_image"
-            className="login_img"
-          />
-        </div>
+        <div className="left"></div>
+        <div className="right"></div>
       </div>
     </div>
   );
