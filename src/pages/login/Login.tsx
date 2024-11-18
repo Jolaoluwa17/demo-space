85371f48ecbe8853ff13ad9b5acc64cabf6f085c
Olusanya <olusanyajolaoluwa@gmail.com>
2024-11-18 16:02:41 +0100

    updates

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,28 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import './login.css';
 import EyeOpen from '@/icons/Eye';
 import EyeClosed from '@/icons/EyeClosed';
-// import { useNavigate } from 'react-router-dom';
-// import {
-//   useLoginMutation,
-//   useRequestCodeMutation,
-// } from '@/services/features/auth/authApiSlice';
-// import { useDispatch } from 'react-redux';
-// import {
-//   setAuthState,
-//   setCredentials,
-// } from '@/services/features/auth/authSlice';
+import {
+  useLoginMutation,
+  useRequestCodeMutation,
+} from '@/services/features/auth/authApiSlice';
+import { useDispatch } from 'react-redux';
+import {
+  setAuthState,
+  setCredentials,
+} from '@/services/features/auth/authSlice';
 
 const Login = () => {
-  // const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  // const [emailError, setEmailError] = useState<boolean>(true);
-  // const isFormValid = password !== '' && !emailError;
+  const [emailError, setEmailError] = useState<boolean>(true);
+  const isFormValid = password !== '' && !emailError;
 
-  // const validateEmail = (email: string) => {
-  //   // Basic email validation regex
-  //   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  // };
+  const validateEmail = (email: string) => {
+    // Basic email validation regex
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
 
   const handleHiddenTrigger = () => {
     setShowPassword(!showPassword);
@@ -34,61 +33,59 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  // const handleEmailChange = (value: string) => {
-  //   setEmail(value);
-  //   if (!validateEmail(value)) {
-  //     setEmailError(true);
-  //   } else {
-  //     setEmailError(false);
-  //   }
-  // };
+  const handleEmailChange = (value: string) => {
+    setEmail(value);
+    if (!validateEmail(value)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
 
-  // const navigator = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useDispatch();
 
-  // const [login, { isLoading }] = useLoginMutation();
-  // const dispatch = useDispatch();
+  const handleLogin = async () => {
+    const userData = {
+      email: email,
+      password: password,
+    };
 
-  // const handleLogin = async () => {
-  //   const userData = {
-  //     email: email,
-  //     password: password,
-  //   };
+    try {
+      const res = await login(userData).unwrap();
+      console.log(res);
+      dispatch(
+        setAuthState({
+          isAuthenticated: true,
+          token: res.token,
+          emailVerified: res.user.status ?? false,
+          id: res.user._id ?? '',
+        })
+      );
+      if (res.user.status === false) {
+        dispatch(setCredentials({ email: email, password: password }));
+        handleRequestCode();
+        navigator('/auth/verifyaccount');
+      } else if (!res.user.fullName) {
+        navigator('/user-profile');
+      } else {
+        navigator('/dashboard');
+      }
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
 
-  //   try {
-  //     const res = await login(userData).unwrap();
-  //     console.log(res);
-  //     dispatch(
-  //       setAuthState({
-  //         isAuthenticated: true,
-  //         token: res.token,
-  //         emailVerified: res.user.status ?? false,
-  //         id: res.user._id ?? '',
-  //       })
-  //     );
-  //     if (res.user.status === false) {
-  //       dispatch(setCredentials({ email: email, password: password }));
-  //       handleRequestCode();
-  //       navigator('/auth/verifyaccount');
-  //     } else if (!res.user.fullName) {
-  //       navigator('/user-profile');
-  //     } else {
-  //       navigator('/dashboard');
-  //     }
-  //   } catch (error: unknown) {
-  //     console.log(error);
-  //   }
-  // };
+  const [requestCode] = useRequestCodeMutation();
 
-  // const [requestCode] = useRequestCodeMutation();
-
-  // const handleRequestCode = async () => {
-  //   const userData = { email: email };
-  //   try {
-  //     await requestCode(userData).unwrap();
-  //   } catch (error: unknown) {
-  //     console.log(error);
-  //   }
-  // };
+  const handleRequestCode = async () => {
+    const userData = { email: email };
+    try {
+      await requestCode(userData).unwrap();
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
 
   const navigator = useNavigate();
 
@@ -110,7 +107,12 @@ const Login = () => {
           </div>
           <div className="login_form_item">
             <label htmlFor="">Enter Email</label>
-            <input type="text" />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => handleEmailChange(e.target.value)}
+              placeholder="ysemiraefe@gmail.com"
+            />
           </div>
           <div className="login_form_item">
             <label htmlFor="">Enter Password</label>
@@ -119,6 +121,7 @@ const Login = () => {
                 type={showPassword ? 'text' : 'password'}
                 value={password}
                 onChange={handlePasswordChange}
+                placeholder="************"
               />
               <div
                 className="login_password_visibility"
@@ -141,18 +144,31 @@ const Login = () => {
               Forgot Password
             </div>
           </div>
-          <button className="login_btn">Login</button>
+          <button
+            className="login_btn"
+            style={{
+              backgroundColor: isFormValid && !isLoading ? '#4274BA' : 'grey',
+              cursor: isFormValid && !isLoading ? 'pointer' : 'not-allowed',
+            }}
+            onClick={handleLogin}
+            disabled={!isFormValid || isLoading}
+          >
+            {isLoading ? <div className="spinner"></div> : 'Login'}
+          </button>
           <div className="donot_have_acc">
             Don’t have an account?{' '}
             <span
               style={{ color: '#FF8682', cursor: 'pointer', fontWeight: '600' }}
+              onClick={() => navigator('/auth/signup')}
             >
               Sign up
             </span>
           </div>
           <div className="or_login">
             <hr />
-            <div style={{ padding: '0px 10px' }}>Or login with</div>
+            <div style={{ padding: '0px 10px', textAlign: 'center' }}>
+              Or login with
+            </div>
             <hr />
           </div>
           <div className="login_options">
@@ -163,14 +179,14 @@ const Login = () => {
               <img src="/assets/images/Apple.svg" alt="login_image" />
             </div>
           </div>
-          <div className='image_container'></div>
+          <div className="image_container"></div>
         </div>
         <div className="right">
-          <img
+          {/* <img
             src="/assets/images/Login.svg"
             alt="login_image"
             className="login_img"
-          />
+          /> */}
         </div>
       </div>
     </div>
