cb5729bdcdf688199f6c60c6705a1268fbf4b7c8
Olusanya <olusanyajolaoluwa@gmail.com>
2025-01-20 13:40:28 +0100

    updates

--- src/pages/user/progress/history/History.tsx
+++ src/pages/user/progress/history/History.tsx
@@ -1,95 +1,140 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
 import './history.css';
 import LeftArrowIcon from '@/icons/LeftArrowIcon';
+import { useGetAllResultsQuery } from '@/services/features/result/resultSlice';
+import { useTotalAttemptsQuery } from '@/services/features/quiz/quizSlice';
+import { FadeLoader } from 'react-spinners';
+import { useEffect } from 'react';
 
 const History = () => {
   const navigate = useNavigate();
-  // const [searchParams] = useSearchParams();
-  // const userId = sessionStorage.getItem('id');
-  // const id = searchParams.get('id');
+  const [searchParams] = useSearchParams();
+  const userId = sessionStorage.getItem('id');
+  const id = searchParams.get('id');
   const location = useLocation();
   const { course, description } = location.state || {};
 
-  // const navigateToQuestion = (questionId: string) => {
-  //   navigate(`/dashboard/progress/question?id=${id}`, {
-  //     state: { course, description, questionId },
-  //   });
-  // };
+  const { data, isLoading, isError, refetch } = useGetAllResultsQuery({});
+  const userResults = data?.response.filter(
+    (result: { quizId: { _id: string }; userId: { _id: string } }) =>
+      result.quizId?._id === id && result.userId?._id === userId
+  );
 
-  // const { data, isLoading } = useGetAllResultsQuery({});
-  // const userResults = data?.response.filter(
-  //   (result: { quizId: { _id: string }; userId: { _id: string } }) =>
-  //     result.quizId?._id === id && result.userId?._id === userId
-  // );
+  const {
+    data: totalAttemptData,
+    isLoading: totalAttemptsLoading,
+    refetch: refetchTotalAttempts,
+    isError: totalAttemptError,
+  } = useTotalAttemptsQuery({ userId: userId, quizId: id });
 
-  // const { data: questionsData, isLoading: questionsLoading } =
-  //   useGetQuizQuestionQuery(id);
+  const attemptsLeft = totalAttemptData?.response?.noOfRetake
+    ? 3 - totalAttemptData.response.noOfRetake
+    : 0;
 
-  // const correctAnswersCount = questionsData?.data.reduce(
-  //   (acc: number, question: { answer: string; userAnswer: string }) => {
-  //     const userAnswer = userResults?.[0]?.answer?.find(
-  //       (ans: string) => question.answer === ans
-  //     );
-  //     return userAnswer === question.answer ? acc + 1 : acc;
-  //   },
-  //   0
-  // );
-
-  // const totalQuestions = questionsData?.data?.length;
+  useEffect(() => {
+    refetch();
+    refetchTotalAttempts();
+  }, [location.key, refetch, refetchTotalAttempts]);
 
   return (
     <div className="history_root">
-      <div className="history_header">
-        <div className="history_inner_header">
-          <div className="history_back_btn" onClick={() => navigate(-1)}>
-            <div className="left_arrow_icon">
-              <LeftArrowIcon />
-            </div>
-            Back
+      {isLoading || totalAttemptsLoading ? (
+        <div className="loading_container">
+          <FadeLoader color="#007BFF" />
+        </div>
+      ) : totalAttemptError || isError ? (
+        <div className="nodata_container">
+          <img
+            src="/images/NoData.jpg"
+            alt="No Data"
+            style={{ width: '250px', height: '250px' }}
+          />
+          <div style={{ fontWeight: '600' }}>
+            Oops, No history at this time 😭
           </div>
         </div>
-      </div>
-      <div className="history_title">
-        <div className="img_holder"></div>
-        <div style={{ marginLeft: '20px' }}>
-          <div className="history_skill_text">{course}</div>
-          <div className="history_skill_description">{description}</div>
+      ) : (
+        <div>
+          <div className="history_header">
+            <div className="history_inner_header">
+              <div className="history_back_btn" onClick={() => navigate(-1)}>
+                <div className="left_arrow_icon">
+                  <LeftArrowIcon />
+                </div>
+                Back
+              </div>
+            </div>
+          </div>
+          <div className="history_title">
+            <div className="img_holder"></div>
+            <div className="history_title_code_text">
+              <div className="history_skill_text">
+                {course}
+                <div className="history_no_of_attempts_left">
+                  No. of attempts left:{' '}
+                  <span style={{ fontWeight: '600' }}>
+                    {totalAttemptError ? 3 : attemptsLeft}
+                  </span>
+                </div>
+              </div>
+              <div className="history_skill_description">{description}</div>
+            </div>
+          </div>
+          <div className="history_table_container">
+            <table className="history_quiz_table">
+              <thead>
+                <tr>
+                  <th>Attempt</th>
+                  <th>Date Taken</th>
+                  <th>Percentage</th>
+                  <th>Passing Grade</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userResults.map(
+                  (
+                    data: { createdAt: string; score: number },
+                    index: number
+                  ) => (
+                    <tr key={index}>
+                      <td className="history_table_first_data">{index + 1}</td>
+                      <td>
+                        {new Date(data.createdAt).toLocaleDateString('en-US', {
+                          year: 'numeric',
+                          month: 'short',
+                          day: 'numeric',
+                        })}
+                      </td>
+                      <td>{data.score}%</td>
+                      <td>70%</td>
+                      <td
+                        style={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          width: '100%',
+                        }}
+                      >
+                        <div
+                          className="history_quiz_status"
+                          style={{
+                            borderColor: data.score < 70 ? '#FF0000' : '',
+                            backgroundColor: data.score < 70 ? '#FFEDED' : '',
+                          }}
+                        >
+                          {data.score < 70 ? 'Fail' : 'Pass'}
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <div className="history_table_container">
-        <table className="history_quiz_table">
-          <thead>
-            <tr>
-              <th>Attempt</th>
-              <th>Date Taken</th>
-              <th>Percentage</th>
-              <th>Correct</th>
-              <th>Passing Grade</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="history_table_first_data">1</td>
-              <td>20/10/2024</td>
-              <td>66%</td>
-              <td>27/40</td>
-              <td>70%</td>
-              <td
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  width: '100%',
-                }}
-              >
-                <div className="history_quiz_status">Pass</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
